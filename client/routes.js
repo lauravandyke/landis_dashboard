17e21ecca80790b465ed08ee75e4e93d8c7a5304
Laura Van Dyke <laura.e.vandyke@gmail.com>
2021-07-28 16:34:50 -0400

    clean up routes

--- client/routes.js
+++ client/routes.js
@@ -1,80 +1,35 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Login, Signup } from './components/AuthForm';
-import Home from './components/Home';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Clients from './components/Clients';
 import SingleClient from './components/SingleClient';
 import EditClient from './components/EditClient';
 import CreateClient from './components/CreateClient';
 import Analytics from './components/Analytics';
-import { me } from './store';
 
-/**
- * COMPONENT
- */
-class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData();
-  }
-
+export default class Routes extends Component {
   render() {
-    const { isLoggedIn } = this.props;
-
     return (
       <div>
-        {isLoggedIn ? (
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Redirect to="/home" />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path="/" exact component={Login} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <Route exact path="/clients" component={Clients} />
-            <Route
-              exact
-              path="/clients/:id"
-              render={(routeProps) => (
-                <SingleClient id={routeProps.match.params.id} />
-              )}
-            />
-            <Route
-              path="/clients/:id/edit"
-              render={(routeProps) => (
-                <EditClient id={routeProps.match.params.id} />
-              )}
-            />
-            <Route path="/new" component={CreateClient} />
-            <Route path="/analytics" component={Analytics} />
-          </Switch>
-        )}
+        <Switch>
+          <Route path="/" exact component={Clients} />
+          <Route exact path="/clients" component={Clients} />
+          <Route
+            exact
+            path="/clients/:id"
+            render={(routeProps) => (
+              <SingleClient id={routeProps.match.params.id} />
+            )}
+          />
+          <Route
+            path="/clients/:id/edit"
+            render={(routeProps) => (
+              <EditClient id={routeProps.match.params.id} />
+            )}
+          />
+          <Route path="/new" component={CreateClient} />
+          <Route path="/analytics" component={Analytics} />
+        </Switch>
       </div>
     );
   }
 }
-
-/**
- * CONTAINER
- */
-const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id,
-  };
-};
-
-const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(me());
-    },
-  };
-};
-
-// The `withRouter` wrapper makes sure that updates are not blocked
-// when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes));
